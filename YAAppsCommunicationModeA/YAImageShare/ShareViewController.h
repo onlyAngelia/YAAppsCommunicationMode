6c35e82109848b620f26af8011060e2f9f698c0f
yxy0720 <479397375@qq.com>
2018-04-25 22:53:35 +0800

    add Share Extension

---
+++ YAAppsCommunicationModeA/YAImageShare/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  YAImageShare
+//
+//  Created by lanjiying on 2018/4/25.
+//  Copyright © 2018年 lanjiying_Angelia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
