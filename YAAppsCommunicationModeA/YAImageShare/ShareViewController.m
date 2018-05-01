f77ee250dd5dc549d3d89496ba8013d4c92f278c
yxy0720 <479397375@qq.com>
2018-05-01 20:12:05 +0800

    handle the post Image

--- YAAppsCommunicationModeA/YAImageShare/ShareViewController.m
+++ YAAppsCommunicationModeA/YAImageShare/ShareViewController.m
@@ -23,6 +23,18 @@
     // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
     // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    NSExtensionItem *extensionContext = self.extensionContext.inputItems[0];
+    if(extensionContext){
+        NSString *contextType = @"YATypeImage";
+        for (NSItemProvider * itemProvider in extensionContext.attachments) {
+            if ([itemProvider hasItemConformingToTypeIdentifier:contextType]) {
+                [itemProvider loadItemForTypeIdentifier:contextType options:nil completionHandler:^(id<NSSecureCoding>  _Nullable item, NSError * _Null_unspecified error) {
+                   //存储到group数据中
+                }];
+            }
+        }
+        
+    }
     [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
 }
 
@@ -30,5 +42,4 @@
     // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
     return @[];
 }
-
 @end
